f0ba7f0f4f1846f9e41bba98ca746acef5ac054e
KarinaNiebles <fkariniebles@gmail.com>
2018-05-30 02:55:58 -0500

    update cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,38 @@
 window.cipher = {
-  // ... 
-};
+  encode: function (text, displace) {
+
+    text = text.toUpperCase();
+    let array = text.split('');
+    let encrypted = '';
+
+    array.forEach(function (element) {
+      let index = element.charCodeAt(0);
+      if (index > 64 && index < 91) {
+        let nextIndex = (index - 65 + displace) % 26 + 65;
+        encrypted += String.fromCharCode(nextIndex);
+      } else {
+        encrypted += element;
+      }
+    });
+    return encrypted;
+  },
+  decode: function (text, displace) {
+    text = text.toUpperCase();
+    let array = text.split('');
+    let encrypted = '';
+
+    array.forEach(function (element) {
+      let index = element.charCodeAt(0);
+      if (index > 64 && index < 91) {
+        let nextIndex = (index - 65) - (displace % 26);
+        if (nextIndex < 0) {
+          nextIndex += 26
+        }
+        encrypted += String.fromCharCode(nextIndex + 65);
+      } else {
+        encrypted += element;
+      }
+    });
+    return encrypted;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+function process(){
+  var string=document.getElementById("texto").value;
+  var offset= parseInt(document.getElementById("displace").value);
+  document.getElementById("result").value = cipher.encode(string,offset);
+}
+/*function process2(){
+  var string=document.getElementById("texto").value;
+  var offset= parseInt(document.getElementById("displace").value);
+  document.getElementById("result").value = cipher.decode(offset,string);
+}*/
+function process2(){
+  var string=document.getElementById("result").value;
+  var offset= parseInt(document.getElementById("displace").value);
+  document.getElementById("xx").value = cipher.decode(string,offset);
+}
